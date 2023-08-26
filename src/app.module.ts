8420eef00658037aede35abef103140c8ed694ca
madikun <4upatai@gmail.com>
2023-08-26 17:24:26 +0600

    ADD mongo(docker), mongoose connection to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [MongooseModule.forRoot('mongodb://localhost/mongo')],
   controllers: [AppController],
   providers: [AppService],
 })
