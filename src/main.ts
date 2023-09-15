14f81ba2ff95ec4c253cb99579a8adc94404b283
Timur <tima@MacBook-Pro.local>
2023-09-15 15:24:13 +0600

    ADD basic swaggerApi logic

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 
 const configSevice = new ConfigService();
@@ -7,6 +8,16 @@ const PORT = configSevice.get('PORT');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Todo example')
+    .setDescription('The Todo API description')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
   console.log(`Server is running on http://localhost:${PORT}`);
 }
